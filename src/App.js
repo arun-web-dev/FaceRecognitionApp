109ec30f7d5985c9765fc20cce4e3d901b35329a
arun-web-dev <miuiandrew@gmail.com>
2022-02-12 23:48:36 +0530

    face recoginition app build success

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Logo from "./components/logo/Logo";
 import "./App.css";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
-
-import FaceRecognition from "../src/components/FaceRecognition/FaceRecognition";
+import Register from "./components/Register/Register.js";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Particles from "react-tsparticles";
+import SignIn from "./components/SignIn/SignIn.js";
 
 import Clarifai, { COLOR_MODEL } from "clarifai";
 
@@ -530,6 +531,9 @@ class App extends Component {
     this.state = {
       input: "",
       imageUrl: "",
+      box: {},
+      route: "signin",
+      isSignedIn: false,
     };
 
     window.addEventListener("load", () => {
@@ -538,37 +542,82 @@ class App extends Component {
       canvasLoad.style.zIndex = -1;
     });
   }
+
+  calculateFaceLocation = (data) => {
+    console.log("i'm called");
+    const clarifyFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const inputImage = document.getElementById("inputImage");
+    const width = Number(inputImage.width);
+    const height = Number(inputImage.height);
+    console.log(width, height);
+
+    return {
+      leftCol: clarifyFace.left_col * width,
+      topRow: clarifyFace.top_row * height,
+      rightCol: width - clarifyFace.right_col * width,
+      bottomRow: height - clarifyFace.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box });
+  };
+
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
   };
-  onButtonSubmit = (e) => {
+
+  onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    console.log(e.target, "click");
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function (response) {
-        console.log(
-          response.outputs[0].data.regions[0].region_info.bounding_box
-        );
-      },
-      function (err) {
-        console.log(err);
-      }
-    );
+
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) => {
+        console.log(response);
+        return this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+      .catch((err) => console.log(err));
+  };
+
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
   };
 
   render() {
     return (
       <div className="App">
         <Particles params={particleOptions} />
-        <Navigation />
-        <Rank />
-        <Logo />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
+        <Navigation
+          isSignedIn={this.state.isSignedIn}
+          onRouteChange={this.onRouteChange}
         />
 
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        {this.state.route === "home" ? (
+          <div>
+            <Rank />
+            <Logo />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+
+            <FaceRecognition
+              box={this.state.box}
+              imageUrl={this.state.imageUrl}
+            />
+          </div>
+        ) : this.state.route === "signin" ? (
+          <SignIn onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,26 @@
 import React from "react";
+import "./FaceRec.css";
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className="center ma">
       <div className="absolute mt2 ">
         <img
+          id="inputImage"
           src={imageUrl}
           alt="face-recognition"
           width="500px"
           height="auto"
         />
+        <div
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol,
+          }}
+          className="bounding-box"
+        ></div>
       </div>
     </div>
   );
